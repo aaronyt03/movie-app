b1170c99985194716fb22473b7f5a7cb0a55d91b
Aaron Lee <alee564@my.bcit.ca>
2021-08-17 10:34:02 -0700

    Create PageSingleMovie.js
    update single movie page

---
+++ src/pages/PageSingleMovie.js
@@ -0,0 +1,9 @@
+function PageSingleMovie() {
+    return (
+        <section className="single-movie-page">
+            <h2>Single Movie Page...</h2>
+        </section>
+    )
+}
+
+export default PageSingleMovie;
